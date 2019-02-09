7cb7173806b597fa48b229b65ce4517c558babe7
LayRT <laylet.rterrazas@gmail.com>
2019-02-08 20:52:57 -0600

    Calculadora con operaciones

---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require ('./math');
+const operaciones = require ('./math');
+console.log("El resultado de la suma es "     + math.suma(10,20));
+console.log("El resultado de la resta es "    +math.resta(10,20));
+console.log("El resultado de la multiplicacion es "     +math.multiplicacion(10,20));
+console.log("El resultado de la division es "     +math.division(10,20));
---
+++ math.js
@@ -0,0 +1,20 @@
+/*function suma(a, b) {
+ return  a + b;         /*Despúes de esta palabra guardará el resultado
+}*/
+
+
+
+module.exports = {
+   suma : function (a, b) {
+    return  a + b;         /*Se guardará en suma*/
+  },
+  resta : function(a,b){
+    return a - b;
+  },
+  multiplicacion : function(a,b){
+    return a * b;
+  },
+  division : function (a,b){
+    return a / b;
+  }
+}
